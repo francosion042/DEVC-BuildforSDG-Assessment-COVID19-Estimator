941edbe2033a694f1ed345de66f937760cad436f
Anthony Nwobodo <anthonynwobodo7@gmail.com>
2020-04-08 02:19:55 +0100

    created and imported the module dollarsInFlight.js

--- src/estimator.js
+++ src/estimator.js
@@ -1,5 +1,6 @@
 import Big from 'big.js';
 import infectionsByRequestedTime from './infectionsByRequestedTime';
+import dollarsInFlight from './dollarsInFlight';
 
 const severeCasesByRequestedTime = ((time) => Math.round(time * 0.15)
 );
@@ -9,28 +10,6 @@ const casesForICUByRequestedTime = ((time) => time * 0.05
 
 const casesForVentilatorsByRequestedTime = ((time) => time * 0.02
 );
-
-const dollarsInFlight = (data, infections) => {
-  let totalDollars;
-  let timeInDays;
-  const { avgDailyIncomePopulation } = data.region;
-  const { avgDailyIncomeInUSD } = data.region;
-  switch (data.periodType) {
-    case 'weeks':
-      timeInDays = data.timeToElapse * 7;
-      totalDollars = infections * avgDailyIncomePopulation * avgDailyIncomeInUSD * timeInDays;
-      break;
-    case 'months':
-      timeInDays = data.timeToElapse * 30;
-      totalDollars = infections * avgDailyIncomePopulation * avgDailyIncomeInUSD * timeInDays;
-      break;
-    default:
-      timeInDays = data.timeToElapse;
-      totalDollars = infections * avgDailyIncomePopulation * avgDailyIncomeInUSD * timeInDays;
-      break;
-  }
-  return totalDollars;
-};
 
 const hospitalBedsByRequestedTime = (data, severeCases) => {
   const availableBeds = data.totalHospitalBeds * 0.35;
@@ -76,42 +55,3 @@ const covid19ImpactEstimator = ((data) => {
 });
 
 export default covid19ImpactEstimator;
-
-
-// const percentage = require('./percentageModule');
-//
-// const covid19ImpactEstimator = (data) => {
-//   const cases = data.reportedCases;
-//   // variable to store the reported cases multiplied by either 10 or 50
-//   const currentlyInfectedOne = cases * 10;
-//   const currentlyInfectedTwo = cases * 50;
-//
-//   const infectionsByRequestedTimeOne = currentlyInfectedOne * 512;
-//   const infectionsByRequestedTimeTwo = currentlyInfectedTwo * 512;
-//
-//   const severeCasesByRequestedTimeOne = percentage(
-//     infectionsByRequestedTimeOne,
-//     15
-//   );
-//   const severeCasesByRequestedTimeTwo = percentage(
-//     infectionsByRequestedTimeTwo,
-//     15
-//   );
-//
-//   const output = {
-//     data: {}, // the input data you got
-//     impact: {
-//       currentlyInfected: currentlyInfectedOne,
-//       infectionsByRequestedTime: infectionsByRequestedTimeOne,
-//       severeCasesByRequestedTime: severeCasesByRequestedTimeOne
-//     }, // your best case estimation
-//     severeImpact: {
-//       currentlyInfected: currentlyInfectedTwo,
-//       infectionsByRequestedTime: infectionsByRequestedTimeTwo,
-//       severeCasesByRequestedTime: severeCasesByRequestedTimeTwo
-//     } // your severe case estimation
-//   };
-//   return output;
-// };
-//
-// export default covid19ImpactEstimator;
