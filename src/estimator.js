a12c77d2f180c8d39086f7f0c8374a84ed98c69a
Anthony Nwobodo <anthonynwobodo7@gmail.com>
2020-04-08 21:27:19 +0100

    modifications

--- src/estimator.js
+++ src/estimator.js
@@ -1,6 +1,6 @@
 const { Big } = require('big.js');
 
-const infectionsByRequestedTime = require('./infectionsByRequestedTime');
+// const infectionsByRequestedTime = require('./infectionsByRequestedTime');
 
 // const dollarsInFlight = require('./dollarsInFlight');
 
@@ -11,6 +11,30 @@ const severeCasesByRequestedTime = ((time) => Math.round(time * 0.15));
 const casesForICUByRequestedTime = ((time) => time * 0.05);
 
 const casesForVentilatorsByRequestedTime = ((time) => time * 0.02);
+
+const infectionsByRequestedTime = (data, currentlyInfected) => {
+  let infections = null;
+  const period = Big(data.timeToElapse);
+  let periodInDays;
+  let unitPeriod;
+  switch (data.periodType) {
+    case 'weeks':
+      periodInDays = period * 7;
+      unitPeriod = Math.floor(periodInDays / 3);
+      infections = currentlyInfected * (2 ** unitPeriod);
+      break;
+    case 'months':
+      periodInDays = period * 30;
+      unitPeriod = Math.floor(periodInDays / 3);
+      infections = currentlyInfected * (2 ** unitPeriod);
+      break;
+    default:
+      unitPeriod = Math.floor(period / 3);
+      infections = currentlyInfected * (2 ** unitPeriod);
+      break;
+  }
+  return infections;
+};
 
 const dollarsInFlight = (data, infections) => {
   let totalDollars;
