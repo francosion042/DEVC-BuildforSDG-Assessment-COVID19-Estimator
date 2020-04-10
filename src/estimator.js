b3df45fad5fde0ce3e7f6c2debb2479b07922a54
Anthony Nwobodo <anthonynwobodo7@gmail.com>
2020-04-10 21:32:54 +0100

    modification

--- src/estimator.js
+++ src/estimator.js
@@ -1,9 +1,18 @@
 const { impactEstimator } = require('./helpers');
 
-const covid19ImpactEstimator = ((data) => {
-  const impact = impactEstimator({ ...data }, 10);
-  const severeImpact = impactEstimator({ ...data }, 50);
-  return { data, impact, severeImpact };
+const covid19ImpactEstimator = (data) => ({
+  data,
+  impact: impactEstimator({ ...data }, 10),
+  severeImpact: impactEstimator({ ...data }, 50)
 });
 
-module.exports = covid19ImpactEstimator;
+const formatAPIResponse = (estimateValues) => covid19ImpactEstimator(estimateValues);
+
+
+const jsonResponse = (request, response) => {
+  const result = request.body;
+  response.status(200).send(formatAPIResponse(result));
+};
+
+
+module.exports = { covid19ImpactEstimator, formatAPIResponse, jsonResponse };
